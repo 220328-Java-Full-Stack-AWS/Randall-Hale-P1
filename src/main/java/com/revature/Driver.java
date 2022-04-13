90f595794d8804fbb3dae9d6743e31c6e2e84c70
randallhale <rahalejr@gmail.com>
2022-04-12 22:38:14 -0700

    Connected minimal user services to database

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,48 @@
 package com.revature;
 
+import com.revature.db.UserDao;
+import com.revature.models.User;
+import com.revature.models.Role;
+import com.revature.services.UserService;
+
+import java.util.Scanner;
+
 public class Driver {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Boolean loggedIn = false;
+
+        while(!loggedIn){
+
+            System.out.println("Welcome. Do you want to login (enter 1) or register (enter 2)?");
+            int response = scan.nextInt();
+            if(response == 1){
+
+                loggedIn = true;
+            }
+            else if(response == 2) {
+                System.out.println("What will your username be?");
+                String username = scan.nextLine();
+                System.out.println("What bout your password?");
+                String password = scan.nextLine();
+                System.out.println("What's your email?");
+                String email = scan.nextLine();
+                System.out.println("Are you an employee (enter 1) or the one in charge (enter 2)?");
+                response = scan.nextInt();
+                if(response == 1) {
+                    UserService.register(username,password,email,Role.valueOf("EMPLOYEE"));
+                }
+                else if(response == 2){
+                    UserService.register(username,password,email,Role.valueOf("FINANCE_MANAGER"));
+                }
+            }
+
+
+        }
+
+
+        }
     }
-}
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,30 +1,30 @@
 package com.revature.models;
+import java.util.List;
 
-/**
- * This concrete User class can include additional fields that can be used for
- * extended functionality of the ERS application.
- *
- * Example fields:
- * <ul>
- *     <li>First Name</li>
- *     <li>Last Name</li>
- *     <li>Email</li>
- *     <li>Phone Number</li>
- *     <li>Address</li>
- * </ul>
- *
- */
+
 public class User extends AbstractUser {
+
+    private List<Reimbursement> reimbursements;
+
+    private String first;
+    private String last;
+    private String email;
 
     public User() {
         super();
     }
 
-    /**
-     * This includes the minimum parameters needed for the {@link com.revature.models.AbstractUser} class.
-     * If other fields are needed, please create additional constructors.
-     */
-    public User(int id, String username, String password, Role role) {
+    public User(int id, String username, String password, Role role, String email) {
         super(id, username, password, role);
+        this.email = email;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
